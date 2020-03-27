11b7753aa3c3ce12335773bf51a8493c18d1fdab
Sanea <sanea@MacBook-Pro-Sanea.local>
2020-03-27 21:50:40 +0200

    lesson28

--- src/components/contactus-form/contactus-form.js
+++ src/components/contactus-form/contactus-form.js
@@ -1,11 +1,11 @@
 import contactUsFormTemplate from './contactus-form.hbs';
 
-const contactUsFormContainer = document.getElementById('conact-form-hbs');
+const contactUsFormContainer = document.getElementById( 'conact-form-hbs' );
 
 const scope = {
-  title: 'Contact US TEMPLATE',
+	title: 'Contact US TEMPLATE',
 };
 
-if (contactUsFormContainer) {
-  contactUsFormContainer.innerHTML = contactUsFormTemplate({ scope });
+if ( contactUsFormContainer ) {
+	contactUsFormContainer.innerHTML = contactUsFormTemplate( { scope } );
 }
--- src/components/header/header.js
+++ src/components/header/header.js
@@ -2,36 +2,39 @@
 
 import headerTemplate from './header.hbs';
 
-var headerContainer = document.getElementById('header-hbs');
+const headerContainer = document.getElementById( 'header-hbs' );
 
-var navigationLinks = [{
-  label: 'Home',
-  url: 'index.html',
-  active: true,
+let navigationLinks = [{
+	label: 'Home',
+	url: 'index.html',
+	active: true,
 }, {
-  label: 'Features',
-  url: '#',
+	label: 'Features',
+	url: '#',
 },
 {
-  label: 'Options',
-  url: '#',
+	label: 'Options',
+	url: '#',
 },
 {
-  label: 'Products',
-  url: 'products',
-},
+	label: 'Products',
+	url: 'products',
+}, {
+	label: 'Contact Us',
+	url: 'contactus',
+}
 ];
 
-navigationLinks = navigationLinks.map((navigationItem) => {
-  if (document.location.pathname.includes(navigationItem.url)) {
-    navigationItem.active = true;
-  } else {
-    navigationItem.active = false;
-  }
+navigationLinks = navigationLinks.map( ( navigationItem ) => {
+	if ( document.location.pathname.includes( navigationItem.url ) ) {
+		navigationItem.active = true;
+	} else {
+		navigationItem.active = false;
+	}
 
-  return navigationItem;
-});
+	return navigationItem;
+} );
 
-if (headerContainer) {
-  headerContainer.innerHTML = headerTemplate({ navigationLinks });
+if ( headerContainer ) {
+	headerContainer.innerHTML = headerTemplate( { navigationLinks } );
 }
